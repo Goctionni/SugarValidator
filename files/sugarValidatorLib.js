c187848e9cd414f7c14c5e815dcf59ca9bd0d4e6
Goctionni <Goctionni@gmail.com>
2019-12-22 21:33:29 +0100

    More validation checks and escape exclusions
    Markup inside of `<nowiki>` `{{{`, `"""` and `<<script>>` is no longer validated
    Widgets in passages without widget tag show as error
    If conditions with `is gte` or alike are now marked as error

--- files/sugarValidatorLib.js
+++ files/sugarValidatorLib.js
@@ -19,6 +19,66 @@ function validate(fileData) {
         }
         return encodedStr;
     }
+
+    function recodeHTML(str) {
+        const recodeMap = Object.keys(entityMap).reduce((map, key) => {
+            const intermediate = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(36) + Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(36);
+            return [
+                ...map,
+                { replace: key, intermediate, find: entityMap[key] },
+            ];
+        }, []);
+        // Find replace find's with intermediate's
+        for (const { replace, intermediate, find } of recodeMap) {
+            while(str.indexOf(find) !== -1) {
+                str = str.replace(find, intermediate);
+            }
+        }
+        // Then replace intermediate's with replace's
+        for (const { replace, intermediate, find } of recodeMap) {
+            while(str.indexOf(intermediate) !== -1) {
+                str = str.replace(intermediate, replace);
+            }
+        }
+        return str;
+    }
+
+    const exclusionMarkup = [
+        ['"""', '"""', 'Markup escape'],
+        ['<nowiki>', '</nowiki>', 'Markup escape'],
+        ['{{{', '}}}', 'Markup escape'],
+        ['<<script>>', '<</script>>', 'Script']
+    ];
+    function recodeExcludedHTML(html, header) {
+        let startIndex = 0;
+        while(true) {
+            let firstIndex = html.length;
+            let firstMarkup = null;
+            for(const markup of exclusionMarkup) {
+                const index = html.indexOf(markup[0], startIndex);
+                if (index !== -1 && index < firstIndex) {
+                    firstIndex = index;
+                    firstMarkup = markup;
+                }
+            }
+            // If none of the opening markup can be found, we're done
+            if (!firstMarkup) {
+                return html;
+            }
+            // If something is found, find the matching closing markup
+            const closingIndex = html.indexOf(firstMarkup[1], firstIndex + firstMarkup[0].length);
+            // If no closing markup is found, add a warning and stop trying to exclude further markup
+            if (closingIndex === -1) {
+                addWarning([
+                    [`Found $$1 opening '$$2', but no subsequent closing '$$3'`, firstMarkup[2], firstMarkup[0], firstMarkup[1]],
+                ], header);
+                return html;
+            }
+            // modify html to re-encode the excluded markup
+            html = html.substr(0, firstIndex + firstMarkup[0].length) + recodeHTML(html.substring(firstIndex + firstMarkup[0].length, closingIndex)) + html.substr(closingIndex);
+            startIndex = closingIndex + firstMarkup[1].length;
+        }
+    }
     
     function getHTMLAttr(html, attr) {
         const startStr = `${attr}="`;
@@ -42,7 +102,7 @@ function validate(fileData) {
         if (endIndex === -1) throw 'Unclosed passage found: ' + fileData.substr(startIndex, 200);
         const contentIndex = fileData.indexOf('>', startIndex) + 1;
         const header = fileData.substring(startIndex, contentIndex);
-        const content = decodeHTML(fileData.substring(contentIndex, endIndex));
+        const content = recodeExcludedHTML(decodeHTML(fileData.substring(contentIndex, endIndex)), header);
         const id = getHTMLAttr(header, 'pid');
         const name = getHTMLAttr(header, 'name');
         const passage = { header, content, id, name };
@@ -63,6 +123,15 @@ function validate(fileData) {
     function addWarning(msg, header) {
         if (!validationWarnings[header]) validationWarnings[header] = [];
         validationWarnings[header].push(msg);
+    }
+
+    function matchQuotes(html, passage) {
+        const numDoubleQuotes = html.split('"').length - 1;
+        const numSingleQuotes = html.split("'").length - 1;
+        const unevenDoubleQuotes = (numDoubleQuotes % 2) === 1;
+        const unevenSingleQuotes = (numSingleQuotes % 2) === 1; // single quotes can reasonably be used in plain english
+        if (unevenDoubleQuotes) addWarning([['Uneven number of double-quotes in passage']], passage.header);
+        // if (unevenSingleQuotes) addWarning([['Uneven number of single-quotes in']], passage.header);
     }
     
     function matchGTLT(html, passage) {
@@ -120,6 +189,20 @@ function validate(fileData) {
             html = html.substr(0, start) + html.substr(end + 7);
         }
     }
+
+    function findInvalidConditions(html, passage) {
+        const matches = html.match(/<<(else)?if [^>]* is(not)? (gt|gte|lt|lte) [^>]*>>/g);
+        if (matches) return throwError(`Invalid condition found: '${matches[0]}', is/isnot should not be used in combination with lt/lte/gt/gte`, passage);
+    }
+
+    function checkInvalidWidgets(html, passage) {
+        // Find our tags
+        const tags = getHTMLAttr(passage.header, 'tags');
+        // If one of this passage's tags is widget, we dont need to check if there are invalid widgets, cause they'd be valid in this passage
+        if (tags.split(' ').indexOf('widget') !== -1) return;
+        // Determine if there are any widget tags
+        if (html.indexOf('<<widget ') !== -1) return throwError('<<widget found in passage without a widget tag', passage);
+    }
     
     function findDeprecatedInPassage(html, passage) {
         const deprecationMap = {
@@ -163,7 +246,6 @@ function validate(fileData) {
         };
         let curIndex = fileData.indexOf('id="twine-user-script"');
         const maxIndex = fileData.indexOf('</script>', curIndex);
-        console.log({curIndex});
         const deprecationKeys = Object.keys(deprecationMap);
         for (const key of deprecationKeys) {
             while(true) {
@@ -184,9 +266,12 @@ function validate(fileData) {
     
     for(const passage of passages) {
         try {
+            matchQuotes(passage.content, passage);
             matchGTLT(passage.content, passage);
             matchIfs(passage.content, passage);
             findDeprecatedInPassage(passage.content, passage);
+            findInvalidConditions(passage.content, passage);
+            checkInvalidWidgets(passage.content, passage);
         } catch(e) {
             errorsFound = true;
         }
